fe759d438467af757c5419911d8cedab3f956561
cloudQuant <yunjinqi@gmail.com>
2024-11-09 23:49:35 +0800

    fix bug

--- tests/test_local_requests.py
+++ tests/test_local_requests.py
@@ -1,24 +1,26 @@
 import requests
+import sys
 
 
-def test_local_requests():
-    result =False
-    try:
-        url = 'https://127.0.0.1:5000/'
-        response = requests.get(url, verify=False)  # 禁用 SSL 验证
-        # response = requests.get(url)
-        if response.status_code == 200:
-            result = True
-    except Exception as e:
+def test_local_requests(params=None):
+    result = False
+    if params is None:
         url = 'https://127.0.0.1:8888/'
         response = requests.get(url, verify=False)  # 禁用 SSL 验证
         # response = requests.get(url)
         if response.status_code == 200:
             result = True
+    if params is not None:
+        url = f'https://{params}/'
+        response = requests.get(url, verify=False, params=params)  # 禁用 SSL 验证
+        if response.status_code == 200:
+            result = True
     assert result is True
-    print(response.text)
     print("local requests test passed")
 
 
 if __name__ == '__main__':
-    test_local_requests()
+    if len(sys.argv) < 2:
+        test_local_requests()
+    url = sys.argv[1]
+    test_local_requests(params=url)
