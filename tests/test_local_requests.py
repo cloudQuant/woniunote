8ea6c4360b243053c42a06f1ae354c19e649abe3
cloudQuant <yunjinqi@gmail.com>
2024-11-09 23:40:09 +0800

    fix bug

--- tests/test_local_requests.py
+++ tests/test_local_requests.py
@@ -2,10 +2,20 @@ import requests
 
 
 def test_local_requests():
-    url = 'https://127.0.0.1:5000/'
-    response = requests.get(url, verify=False)  # 禁用 SSL 验证
-    # response = requests.get(url)
-    assert response.status_code == 200
+    result =False
+    try:
+        url = 'https://127.0.0.1:5000/'
+        response = requests.get(url, verify=False)  # 禁用 SSL 验证
+        # response = requests.get(url)
+        if response.status_code == 200:
+            result = True
+    except Exception as e:
+        url = 'https://127.0.0.1:8888/'
+        response = requests.get(url, verify=False)  # 禁用 SSL 验证
+        # response = requests.get(url)
+        if response.status_code == 200:
+            result = True
+    assert result is True
     print(response.text)
     print("local requests test passed")
 
