bb42d65f4e500e7bc397dad4ad4ec67167d11cd9
cloudQuant <yunjinqi@gmail.com>
2024-11-09 23:51:26 +0800

    fix bug

--- tests/test_local_requests.py
+++ tests/test_local_requests.py
@@ -6,12 +6,14 @@ def test_local_requests(params=None):
     result = False
     if params is None:
         url = 'https://127.0.0.1:8888/'
+        print(f"begin to request {url}")
         response = requests.get(url, verify=False)  # 禁用 SSL 验证
         # response = requests.get(url)
         if response.status_code == 200:
             result = True
     if params is not None:
         url = f'https://{params}/'
+        print(f"begin to request {url}")
         response = requests.get(url, verify=False, params=params)  # 禁用 SSL 验证
         if response.status_code == 200:
             result = True
@@ -22,5 +24,5 @@ def test_local_requests(params=None):
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         test_local_requests()
-    url = sys.argv[1]
-    test_local_requests(params=url)
+    local_url = sys.argv[1]
+    test_local_requests(params=local_url)
