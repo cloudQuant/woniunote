078693f9feed0d877d958aa924d957244c6f8c9b
cloudQuant <yunjinqi@gmail.com>
2025-01-06 13:56:35 +0800

    fix type

--- woniunote/controller/article.py
+++ woniunote/controller/article.py
@@ -107,7 +107,8 @@ def pre_post():
 def go_edit(articleid):
     try:
         result = Articles().find_by_id(articleid)
-        print("go_edit", articleid, result)
+        print("go_edit", articleid)
+        print("result = ", result)
         target_html = "article-edit.html"
         article_type = ARTICLE_TYPES
         return render_template(target_html, result=result, article_type=article_type)
