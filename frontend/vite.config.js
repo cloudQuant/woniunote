b113c80fbe9de9448a1b9188b7d21c8ec62c45eb
yunjinqi <yunjinqi@gnail.com>
2025-12-25 22:47:58 +0800

    fix allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,8 +13,14 @@ export default defineConfig(({ command }) => ({
     host: '0.0.0.0',
     port: 8888,
     allowedHosts: ['yunjinqi.top', 'www.yunjinqi.top', 'localhost', '127.0.0.1'],
-    // 完全禁用 HMR WebSocket 连接
-    hmr: false,
+    // // 完全禁用 HMR WebSocket 连接
+    // hmr: false,
+     // 关键配置 ↓↓↓
+    hmr: {
+      // 方案1A：使用反向代理
+      host: 'www.yunjinqi.top',
+      port: 443,  // 如果使用了SSL证书
+    },
     watch: {
       usePolling: false
     },
@@ -27,6 +33,10 @@ export default defineConfig(({ command }) => ({
         target: 'http://localhost:5173',
         changeOrigin: true
       }
+    },
+    // 添加头部解决跨域
+    headers: {
+      'Access-Control-Allow-Origin': '*',
     }
   },
   // 生产构建时不需要 HMR
